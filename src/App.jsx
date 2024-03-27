3274794d04b4810ef4f1444c21892b838b3696f9
Erikwira <dashfly136@gmail.com>
2024-03-27 13:10:32 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Hallo, Saya Erik Narendra Wiraditya</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
