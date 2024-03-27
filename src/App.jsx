e66dde4f5392f2b66f60c313c9bac6ed1a38a45e
Erikwira <dashfly136@gmail.com>
2024-03-27 13:57:07 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import List from "./pages/list/List";
-import Single from "./pages/single/Single";
-import New from "./pages/new/New";
+import Home from "./pages/home/Home.jsx";
+import Login from "./pages/login/Login.jsx";
+import List from "./pages/list/List.jsx";
+import Single from "./pages/single/Single.jsx";
+import New from "./pages/new/New.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
