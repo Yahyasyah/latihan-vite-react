fe8693f1fffad74b005939e66220a281a737a776
Erikwira <dashfly136@gmail.com>
2024-03-27 14:26:28 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import Home from "./pages/home/Home.jsx";
-import Login from "./pages/login/Login.jsx";
-import List from "./pages/list/List.jsx";
-import Single from "./pages/single/Single.jsx";
-import New from "./pages/new/New.jsx";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,6 +15,11 @@ function App() {
             <Route index element={<Home />}></Route>
             <Route path="login" element={<Login />}></Route>
             <Route path="users">
+              <Route index element={<List />}></Route>
+              <Route path=":userId" element={<Single />}></Route>
+              <Route path="new" element={<New />}></Route>
+            </Route>
+            <Route path="products">
               <Route index element={<List />}></Route>
               <Route path=":userId" element={<Single />}></Route>
               <Route path="new" element={<New />}></Route>
