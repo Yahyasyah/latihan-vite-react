18354ba3dcb857b4120c0f6c01d5b7a01096f6c1
Erikwira <dashfly136@gmail.com>
2024-05-22 13:52:57 +0700

    Menambahkan Categories

--- src/pages/myList/MyList.jsx
+++ src/pages/myList/MyList.jsx
@@ -14,6 +14,8 @@ const MyList = () => {
         <div className="widgets">
           <Widget type="user"/>
           <Widget type="order"/>
+          <Widget type="earning" />
+          <Widget type="earning" />
         </div>
         <div className="table">
           <MyDatatable title="All Data" />
